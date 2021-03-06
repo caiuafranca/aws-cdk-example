3973769de0f14bc3a9767ca0e1d02854bb7ecfda
Caiua França <caiuagomes@gmail.com>
2021-03-06 20:45:46 -0300

    add redshift and vpc

--- aws_cdk_explample/aws_cdk_explample_stack.py
+++ aws_cdk_explample/aws_cdk_explample_stack.py
@@ -1,5 +1,7 @@
 from aws_cdk import core
 from aws_cdk import aws_s3 as S3
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_redshift as _redshift
 
 
 class AwsCdkExplampleStack(core.Stack):
@@ -7,3 +9,17 @@ class AwsCdkExplampleStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         S3.Bucket(self, 'bucket-bootcamp04-test', bucket_name='bucket-bootcamp04-test')
+
+        vpc = ec2.Vpc(self, 'myVpc', cidr='0.0.0.0/16')
+
+        cluster = _redshift.Cluster(self, "Redshift",
+            master_user=Login(
+                master_username="admin"
+            ),
+            vpc=vpc
+        )
+        cluster.connections.allow_from_any_ipv4("Open to the world")
+        cluster.cluster_endpoint.socket_address
+        cluster.add_rotation_multi_user("admin",
+            secret="Admin1234"
+        )
