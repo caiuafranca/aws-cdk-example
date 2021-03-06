954eb3523d42ad97efd245b65f3d7f5ac850eded
Caiua França <caiuagomes@gmail.com>
2021-03-06 19:04:01 -0300

    Add stack redshift

--- aws_cdk_explample/aws_cdk_explample_stack.py
+++ aws_cdk_explample/aws_cdk_explample_stack.py
@@ -1,9 +1,20 @@
 from aws_cdk import core
+from aws_cdk import aws_s3 as S3
+from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_redshift as redshift
 
 
 class AwsCdkExplampleStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        S3.Bucket(self, 'bucket-bootcamp04-test', bucket_name='bucket-bootcamp04-test')
 
-        # The code that defines your stack goes here
+        vpc = ec2.Vpc(self, 'vpc', cidr='0.0.0.0/0', id='vpc')
+
+        redshift.Cluster(
+            self, 'redshift-bootcamp03-test', 
+            master_user= "admin",
+            cluster_name='redshift-bootcamp03-test',
+            vpc=vpc
+        )
